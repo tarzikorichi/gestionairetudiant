bb64be87bd86f8c9f8d0fa0eb4061cf5dfbc8be0
Korichi Tarzi <45874148+tarzikorichi@users.noreply.github.com>
2020-10-05 22:26:14 +0100

    add database type sqlite and jar fil in root directorie and add select all or select using where.

--- gestionairetudiant/src/gestionairetudiant/DBConnexion.java
+++ gestionairetudiant/src/gestionairetudiant/DBConnexion.java
@@ -5,31 +5,135 @@
  */
 package gestionairetudiant;
 
-import java.sql.*;
-import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  *
  * @author Elias
  */
 public class DBConnexion {
-    static String username="root"; 
-    static String password="";
-    static String con_string="jdbc:mysql://localhost/tpdatabase";
-    static Connection con=null;
+    Connection con = null;
+    Statement stat = null;
     
-    public  static Connection ConnectDB(){
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = (Connection) DriverManager.getConnection(con_string,username,password);
-            System.out.println("Connected");
-       
-            return con;
-        } catch (ClassNotFoundException | SQLException e) {
-            JOptionPane.showMessageDialog(null,e);
-            return null;
-        }}
-
-    PreparedStatement prepareStatement(String sql) {return (null);
- }
+    public DBConnexion(){
+        try{
+            con = DriverManager.getConnection("jdbc:sqlite:dbEtudient.db");
+            System.out.println("connection ok ... ");
+        }catch(Exception e){
+            System.err.println("Error: "+ e.getMessage());
+        }
+    }
+    
+    public void closeConnection(){
+        try{
+            this.con.close();
+        }catch(Exception e){
+            System.err.println("Error: "+ e.getMessage());
+        }
+    }
+    
+    // select all from table, use where argement where="id = 1"
+    public String SelectAdmin(String where){
+        String res = "";
+        try{
+            stat = con.createStatement();
+            ResultSet rs = stat.executeQuery("SELECT  FROM admin "+where);
+            while(rs.next()){
+                res += rs.getInt("id")+","+rs.getString("nom")+","+rs.getString("password")+"-";
+            }
+            return res;
+        }catch(Exception e){
+            System.err.println("Error: "+ e.getMessage());
+            return res;
+        }
+    }
+    public String SelectEtudients(String where){
+        String res = "";
+        try{
+            stat = con.createStatement();
+            ResultSet rs = stat.executeQuery("SELECT  FROM Etudients "+where);
+            while(rs.next()){
+                res += rs.getInt("id")+","+rs.getString("nomEtPrenom")+","+rs.getString("email")+","+rs.getString("niveau")+"-";
+            }
+            return res;
+        }catch(Exception e){
+            System.err.println("Error: "+ e.getMessage());
+            return res;
+        }
+    }
+    public String SelectdepChef(String where){
+        String res = "";
+        try{
+            stat = con.createStatement();
+            ResultSet rs = stat.executeQuery("SELECT * FROM depChef "+where);
+            while(rs.next()){
+                res += rs.getInt("id")+","+rs.getString("nomEtPrenom")+","+rs.getString("Departement")+"-";
+            }
+            return res;
+        }catch(Exception e){
+            System.err.println("Error: "+ e.getMessage());
+            return res;
+        }
+    }
+    public String SelectGroup(String where){
+        String res = "";
+        try{
+            stat = con.createStatement();
+            ResultSet rs = stat.executeQuery("SELECT * FROM group "+where);
+            while(rs.next()){
+                res += rs.getInt("id")+","+rs.getString("nomGr")+"-";
+            }
+            return res;
+        }catch(Exception e){
+            System.err.println("Error: "+ e.getMessage());
+            return res;
+        }
+    }
+    public String SelectModule(String where){
+        String res = "";
+        try{
+            stat = con.createStatement();
+            ResultSet rs = stat.executeQuery("SELECT * FROM module "+where);
+            while(rs.next()){
+                res += rs.getInt("id")+","+rs.getString("nom")+"-";
+            }
+            return res;
+        }catch(Exception e){
+            System.err.println("Error: "+ e.getMessage());
+            return res;
+        }
+    }
+    public String SelectNote(String where){
+        String res = "";
+        try{
+            stat = con.createStatement();
+            ResultSet rs = stat.executeQuery("SELECT * FROM note "+where);
+            while(rs.next()){
+                res += rs.getInt("id")+","+rs.getInt("valuer")+"-";
+            }
+            return res;
+        }catch(Exception e){
+            System.err.println("Error: "+ e.getMessage());
+            return res;
+        }
+    }
+     public String SelectUnity(String where){
+        String res = "";
+        try{
+            stat = con.createStatement();
+            ResultSet rs = stat.executeQuery("SELECT * FROM unity "+where);
+            while(rs.next()){
+                res += rs.getInt("id")+","+rs.getString("nom")+"-";
+            }
+            return res;
+        }catch(Exception e){
+            System.err.println("Error: "+ e.getMessage());
+            return res;
+        }
+    }
 }
+
+
